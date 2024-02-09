8561a36024043a0ec302df11d8988720c11062c3
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-02-09 14:34:11 -0300

    Security
    security config

---
+++ src/test/java/com/fbert/TaskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbert.TaskManager;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
